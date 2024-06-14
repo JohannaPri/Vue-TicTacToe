3da616ee40958eef4e58448ec937f0f5da801d76
Johanna Prinz <Johannaa.L@hotmail.com>
2024-06-14 15:06:12 +0200

    📝 Added comments in GameBoard file

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -3,21 +3,26 @@
   import SingleSquare from './SingleSquare.vue';
   import { IGameState } from '../models/IGameState';
 
+  // Definiera props med typ för gameState från IGameState
   const props = defineProps<{
     gameState: IGameState;
   }>();
 
+  // Definiera emit-händelsen för 'play'
   const emit = defineEmits(["play"]);
 
   let currentUser = props.gameState.users.nameX;
 
+  // Funktion för att hantera ett speldrag
   const play = (index: number) => {
+    // Om spelet är över, returnera
     if (props.gameState.gameOver) {
       return;
     }
 
     const { gameboard } = props.gameState;
     if (gameboard[index].length === 0) {
+      // Uppdatera spelbrädet beroende på vilken tur det är
       if (props.gameState.isXturn) {
         gameboard[index] = "X";
         currentUser = props.gameState.users.nameO;
@@ -25,14 +30,18 @@
         gameboard[index] = "O";
         currentUser = props.gameState.users.nameX;
       }
+      // Växla spelarens tur
       props.gameState.isXturn = !props.gameState.isXturn;
 
+      // Kontrollera om spelet är över
       checkIfGameOver(index);
+      // Emit händelse när ett drag har gjorts
       emit("play", { index, player: gameboard[index] });
     }
   };
 
   function checkIfGameOver(index: number) {
+     // Definiera alla vinnande kombinationer
     const winningCombinations = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], 
       [0, 3, 6], [1, 4, 7], [2, 5, 8], 
@@ -41,6 +50,7 @@
 
     const currentPlayer = props.gameState.gameboard[index];
 
+    // Kontrollera om det finns en vinnande kombination
     for (const combination of winningCombinations) {
       const [a, b, c] = combination;
       if (
@@ -48,17 +58,19 @@
         props.gameState.gameboard[b] === currentPlayer &&
         props.gameState.gameboard[c] === currentPlayer
       ) {
+        // Uppdatera poängen beroende på vinnaren
         if (currentPlayer == "O") {
           props.gameState.scores.scoresO++;
         } else {
           props.gameState.scores.scoresX++;
         }
+        // Markera spelet som över
         props.gameState.gameOver = true;
         break;
       }
     }
 
-    
+    // Kontrollera om det är oavgjort
     const isTie = props.gameState.gameboard.every((square) => square !== "");
     if (isTie) {
       props.gameState.gameOver = true;
@@ -67,8 +79,10 @@
 </script>
 
 <template>
+  <!-- Visa vems tur det är om spelet inte är över, annars visa 'Game Over' -->
   <h1 v-if="!gameState.gameOver">Det är {{ currentUser }}s tur!</h1>
   <h1 v-else>Game Over!</h1>
+  <!-- Visa spelbrädet -->
   <div class="gameBoard">
     <SingleSquare
       v-for="(square, index) in gameState.gameboard"
