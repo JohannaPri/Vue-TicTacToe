d7ed7c7e9157aeb7c9ceffdc22c4cd13d59cfe09
Johanna Prinz <Johannaa.L@hotmail.com>
2024-06-14 13:44:52 +0200

    ✨ Added import TictactoeGame

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import TictactoeGame from './components/TictactoeGame.vue';
 
 </script>
 
 <template>
-  
+ <div>
+  <TictactoeGame />
+ </div>
 </template>
 
 <style scoped>
+    div {
+        width: auto;
+    }
 
+    @media (width <= 740px) {
+        div {
+            scale: 0.9;
+            max-width: 22rem;
+        }
+    }
 </style>
